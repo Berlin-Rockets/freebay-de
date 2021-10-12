1f0c099e8ce7395e5a0f039a6b78ecbc380cf616
Stan-Getz <stanislav.genunchi@gmail.com>
2021-10-13 01:43:38 +0200

    Try another way around

--- app.js
+++ app.js
@@ -23,14 +23,14 @@ app.use(express.json());
 app.use('/users', userRoute);
 app.use('/api/items', itemRoute);
 
-// if (process.env.NODE_ENV === 'production') {
-//   // Set static folder
-//   app.use(express.static('client/build'));
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
 
-//   app.get('*', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-//   });
-// }
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 // app.use((error, req, res, next) => {
 //   res.json(error.toString());
 // });
