b8c252faab229f8f407e9be5954f1933dd8f2dfa
Stan-Getz <stanislav.genunchi@gmail.com>
2021-10-13 01:30:18 +0200

    Try to deploy while commented out

--- app.js
+++ app.js
@@ -23,17 +23,17 @@ app.use(express.json());
 app.use('/users', userRoute);
 app.use('/api/items', itemRoute);
 
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
+// if (process.env.NODE_ENV === 'production') {
+//   // Set static folder
+//   app.use(express.static('client/build'));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
-app.use((error, req, res, next) => {
-  res.json(error.toString());
-});
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//   });
+// }
+// app.use((error, req, res, next) => {
+//   res.json(error.toString());
+// });
 
 app.listen(port || 4000, () => {
   console.log('====================================');
