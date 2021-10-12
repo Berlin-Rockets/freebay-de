62ea870b3db02ea565095cfc4daf0c01ca0fe748
Stan-Getz <stanislav.genunchi@gmail.com>
2021-10-13 00:39:38 +0200

    Added path to required variables

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const { port, mongoURL } = require("./config/env");
-const userRoute = require("./routes/UserRoutes");
-const itemRoute = require("./routes/ItemRoutes");
-const core = require("./middleware/security");
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const cors = require('cors');
+const { port, mongoURL } = require('./config/env');
+const userRoute = require('./routes/UserRoutes');
+const itemRoute = require('./routes/ItemRoutes');
+const core = require('./middleware/security');
 
 mongoose.connect(mongoURL, {
   useNewUrlParser: true,
@@ -14,22 +15,20 @@ mongoose.connect(mongoURL, {
 
 const app = express();
 
-
-
 app.use(core);
-app.use(cors({ origin: "*", exposedHeaders: "token" }));
+app.use(cors({ origin: '*', exposedHeaders: 'token' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use("/users", userRoute);
-app.use("/api/items", itemRoute);
+app.use('/users', userRoute);
+app.use('/api/items', itemRoute);
 
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static("client/build"));
+  app.use(express.static('client/build'));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 app.use((error, req, res, next) => {
@@ -37,7 +36,7 @@ app.use((error, req, res, next) => {
 });
 
 app.listen(port || 4000, () => {
-  console.log("====================================");
-  console.log("Server start with port: " + port);
-  console.log("====================================");
+  console.log('====================================');
+  console.log('Server start with port: ' + port);
+  console.log('====================================');
 });
